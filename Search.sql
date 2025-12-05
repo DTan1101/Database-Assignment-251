e0bd61a9f25bb51871bfb5424a94b19e282c6cca
DTan1101 <michael.duytan1101@gmail.com>
2025-12-05 15:15:33 +0700

    [update] Search.sql

--- Search.sql
+++ Search.sql
@@ -1,48 +1,63 @@
-USE TutorSS
+USE TutorSS;
 GO
 
-CREATE OR ALTER PROCEDURE sp_TimKiemLopHocNangCao
-    @TenMonHoc NVARCHAR(50) = NULL,      
-    @KhuVuc NVARCHAR(50) = NULL,         
-    @MucLuongMin DECIMAL(18,0) = NULL,   
-    @MucLuongMax DECIMAL(18,0) = NULL,
-    @TrangThai NVARCHAR(50) = NULL,      
-    @SortBy INT = 1                      
+CREATE OR ALTER PROCEDURE sp_TimKiemQuizNangCao
+    @TenQuiz NVARCHAR(100) = NULL,
+    @TenMonHoc NVARCHAR(100) = NULL,
+    @TenGiaSu NVARCHAR(100) = NULL,
+    @TrangThai NVARCHAR(20) = NULL, 
+    @LopDay NVARCHAR(50) = NULL,    
+    @TuNgay DATETIME = NULL,        
+    @DenNgay DATETIME = NULL,       
+    @SoLanLamMin INT = NULL,        
+    
+    @SortBy INT = 1                
 AS
 BEGIN
     SELECT 
-        lh.lop_hoc_id AS MaLop,          
-        mh.ten_mon_hoc AS MonDay,
+        q.ten_quiz AS TenQuiz,
+        q.so_lan_duoc_lam,
+        q.thoi_gian_mo,
+        q.thoi_gian_dong,
         lh.lop_day AS Lop,
-        lh.muc_luong,
-        lh.dia_chi,
-        lh.so_buoi,
-        lh.yeu_cau,
-        lh.trang_thai_giao
+        mh.ten_mon_hoc AS MonHoc,
+        gs.ho_ten AS GiaoVien,
+        CASE 
+            WHEN GETDATE() < q.thoi_gian_mo THEN N'Chưa mở'
+            WHEN GETDATE() > q.thoi_gian_dong THEN N'Đã đóng'
+            ELSE N'Đang mở'
+        END AS TrangThaiHienTai
     FROM 
-        lop_hoc lh                       
+        quiz q
+    JOIN 
+        lop_hoc lh ON q.lop_hoc_id = lh.lop_hoc_id
     LEFT JOIN 
         day d ON lh.lop_hoc_id = d.lop_hoc_id
     LEFT JOIN 
-        mon_hoc mh ON d.mon_hoc_id = mh.mon_hoc_id 
+        mon_hoc mh ON d.mon_hoc_id = mh.mon_hoc_id
+    LEFT JOIN 
+        gia_su gs ON lh.gia_su_id = gs.gia_su_id
     WHERE 
-        (@TenMonHoc IS NULL OR mh.ten_mon_hoc LIKE N'%' + @TenMonHoc + '%')
-        AND (@KhuVuc IS NULL OR lh.dia_chi LIKE N'%' + @KhuVuc + '%')
-        AND (@MucLuongMin IS NULL OR lh.muc_luong >= @MucLuongMin)
-        AND (@MucLuongMax IS NULL OR lh.muc_luong <= @MucLuongMax)
-        AND (@TrangThai IS NULL OR lh.trang_thai_giao = @TrangThai)
+        (@TenQuiz IS NULL OR q.ten_quiz LIKE N'%' + @TenQuiz + '%')
+        AND (@TenMonHoc IS NULL OR mh.ten_mon_hoc LIKE N'%' + @TenMonHoc + '%')
+        AND (@TenGiaSu IS NULL OR gs.ho_ten LIKE N'%' + @TenGiaSu + '%')
+        AND (
+            @TrangThai IS NULL 
+            OR (@TrangThai = 'DangMo' AND GETDATE() BETWEEN q.thoi_gian_mo AND q.thoi_gian_dong)
+            OR (@TrangThai = 'DaDong' AND GETDATE() > q.thoi_gian_dong)
+            OR (@TrangThai = 'ChuaMo' AND GETDATE() < q.thoi_gian_mo)
+        )
+        AND (@LopDay IS NULL OR lh.lop_day LIKE N'%' + @LopDay + '%')
+        AND (@TuNgay IS NULL OR q.thoi_gian_mo >= @TuNgay)
+        AND (@DenNgay IS NULL OR q.thoi_gian_mo <= @DenNgay)
+        AND (@SoLanLamMin IS NULL OR q.so_lan_duoc_lam >= @SoLanLamMin)
+
     ORDER BY 
-        CASE WHEN @SortBy = 1 THEN lh.muc_luong END ASC,
-        CASE WHEN @SortBy = 2 THEN lh.muc_luong END DESC,
-        CASE WHEN @SortBy = 3 THEN lh.lop_hoc_id END DESC;
+        CASE WHEN @SortBy = 1 THEN q.ten_quiz END ASC,
+        CASE WHEN @SortBy = 2 THEN q.thoi_gian_dong END DESC,
+        CASE WHEN @SortBy = 3 THEN q.thoi_gian_mo END DESC; 
 END;
 GO
 
-EXEC sp_TimKiemLopHocNangCao 
-    @TenMonHoc = N'Toán', 
-    @MucLuongMin = 4000000, 
-    @SortBy = 2; 
-
-EXEC sp_TimKiemLopHocNangCao 
-    @KhuVuc = N'Q.10', 
-    @SortBy = 1;
+EXEC sp_TimKiemQuizNangCao
+    @TenMonHoc = N'Tiếng Việt (Tiểu học)';
